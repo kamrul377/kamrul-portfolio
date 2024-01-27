4a657c8d86aa91e9b727ef25f850b8f314a512f9
kamrul islam <mdkamrulislam9810@gmail.com>
2024-01-27 15:09:24 +0600

    add profile

--- src/utils/Social.jsx
+++ src/utils/Social.jsx
@@ -6,22 +6,22 @@ import { TbWorldWww } from "react-icons/tb";
 import { FaTwitter } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
-const Social = ({className}) => {
+const Social = ({ className }) => {
     return (
         <div className={`icons flex gap-7 ${className}`}>
-            <Link className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black ">
+            <Link to={'https://web.facebook.com/profile.php?id=100034572162788'} className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black" target='_black'>
                 <FaSquareFacebook size={25} className='hover:animate-pulse' />
             </Link>
             <Link className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black">
-                <FaTwitter size={25} className='hover:animate-pulse' />
+                <FaTwitter size={25} className='hover:animate-pulse' target='_blank' />
             </Link>
-            <Link className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black">
+            <Link to={'https://github.com/kamrul377'} className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black" target='_blank'>
                 <FaSquareGithub size={25} className='hover:animate-pulse' />
             </Link>
-            <Link className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black">
+            <Link to={'https://www.linkedin.com/in/kamrulislam377/'} className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black" target='_blank'>
                 <IoLogoLinkedin size={25} className='hover:animate-pulse' />
             </Link>
-            <Link className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black">
+            <Link to={'https://mdkamrulislam.vercel.app/'} className="facebook p-3 bg-[#0f172a] rounded shadow border border-slate-800 text-white icn-dsn hover:text-black" target='_blank'>
                 <TbWorldWww size={25} className='hover:animate-pulse' />
             </Link>
 
